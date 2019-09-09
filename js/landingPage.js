4f466935cecd02699cd4928300e990eef4c8ec7e
BenKats <katsbenjamin@gmail.com>
2019-09-09 14:42:36 -0400

    Created Signup function succesfully

--- js/landingPage.js
+++ js/landingPage.js
@@ -6,15 +6,30 @@ signinBttn.addEventListener('click', newUser);
 function newUser(e) {
     e.preventDefault();
 
-    console.log(e.value);
-    const user = document.getElementById('username');
-    const pass = document.getElementById('id');
-    const email = document.getElementById('email');
-    callSignup(user, pass, email);
+    console.log(e);
+    let user = document.getElementById('username');
+    console.log(user);
+    console.log(user.value);
+    let pass = document.getElementById('password');
+    console.log(pass);
+    console.log(pass.value);
+    let email = document.getElementById('email');
+    callSignup(user.value, pass.value, email.value);
+    console.log(email.value);
 }
 
-function callSignup(user, pass, email) {
+function callSignup(user, pass, emailAddress) {
     fetch('http://thesi.generalassemb.ly:8080/signup', {
-        method: 'POST'
-    });
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: user, password: pass, email: emailAddress })
+    })
+        .then(res => {
+            console.log(res);
+            return res;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    console.log('MADE A NEW USER');
 }
