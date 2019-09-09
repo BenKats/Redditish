b8344dd13c0b3250afdecca8ac21cff371828c8e
BenKats <katsbenjamin@gmail.com>
2019-09-09 16:42:09 -0400

    Added function to recognize login input and updated html ids

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,35 +1,53 @@
 console.log('Landing Page JS connected');
 
-const signinBttn = document.getElementById('signup-bttn');
-signinBttn.addEventListener('click', newUser);
+const signupBttn = document.getElementById('signup-bttn');
+const loginBttn = document.getElementById('login-bttn');
+let username = null;
+let password = null;
+let email = null;
+let token = null;
+signupBttn.addEventListener('click', newUser);
+loginBttn.addEventListener('click', returningUser);
 
 function newUser(e) {
     e.preventDefault();
 
-    console.log(e);
-    let user = document.getElementById('username');
-    console.log(user);
-    console.log(user.value);
-    let pass = document.getElementById('password');
-    console.log(pass);
-    console.log(pass.value);
-    let email = document.getElementById('email');
-    callSignup(user.value, pass.value, email.value);
-    console.log(email.value);
+    // console.log(e);
+    username = document.getElementById('username');
+    // console.log(username);
+    // console.log(username.value);
+    password = document.getElementById('signup-password');
+    // console.log(password);
+    // console.log(password.value);
+    email = document.getElementById('signup-email');
+    callSignup(username.value, password.value, email.value);
+    // console.log(email.value);
 }
 
-function callSignup(user, pass, emailAddress) {
+function callSignup(username, password, email) {
     fetch('http://thesi.generalassemb.ly:8080/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: user, password: pass, email: emailAddress })
+        body: JSON.stringify({ username: username, password: password, email: email })
     })
         .then(res => {
             console.log(res);
             return res;
         })
+        .then(res => {
+            console.log('Whats inside?');
+            console.log(res.json());
+            token = res.json();
+        })
         .catch(error => {
             console.error(error);
         });
     console.log('MADE A NEW USER');
+}
+
+function returningUser(e) {
+    e.preventDefault();
+    email = document.getElementById('login-email');
+    password = document.getElementById('login-password');
+    callLogin(email, password);
 }
