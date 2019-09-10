227ba0b265f5cd71361bd61ab34011908cece1fd
Janna Pierre <jannapierre@gmail.com>
2019-09-10 10:11:35 -0400

    Created function for inputtext to display on body.

--- js/homePage.js
+++ js/homePage.js
@@ -1,6 +1,21 @@
 //click on button and window appears
 function postWindow() {
   let postBoxWindow = document.getElementById("postBox");
-
   postBoxWindow.style.display = "block";
+}
+
+//click post button then postBox will appear on body
+function postBoxComplete() {
+  let displayPostTitle = document.createElement("div");
+  displayPostTitle.innerText = document.getElementById("postTitle").value;
+  document.body.appendChild(displayPostTitle);
+
+  //input for textBox to display
+  let displayTextBox = document.createElement("div");
+  displayTextBox.innerText = document.getElementById("textBox").value;
+  document.body.appendChild(displayTextBox);
+
+  // close postBox after submit
+  let postBoxWindow = document.getElementById("postBox");
+  postBoxWindow.style.display = "none";
 }
