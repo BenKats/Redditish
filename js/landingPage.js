315679b613d196bb70b0d681ea7dd956f4653b48
BenKats <katsbenjamin@gmail.com>
2019-09-10 00:29:13 -0400

    Linted code

--- js/landingPage.js
+++ js/landingPage.js
@@ -2,42 +2,36 @@ console.log('Landing Page JS connected');
 
 const signupBttn = document.getElementById('signup-bttn');
 const loginBttn = document.getElementById('login-bttn');
-let username = null;
-let password = null;
-let email = null;
 let token = null;
 signupBttn.addEventListener('click', newUser);
 loginBttn.addEventListener('click', returningUser);
 
 function newUser(e) {
     e.preventDefault();
-
-    // console.log(e);
-    username = document.getElementById('username');
-    // console.log(username);
-    // console.log(username.value);
-    password = document.getElementById('signup-password');
-    // console.log(password);
-    // console.log(password.value);
-    email = document.getElementById('signup-email');
+    let username = document.getElementById('username');
+    let password = document.getElementById('signup-password');
+    let email = document.getElementById('signup-email');
     callSignup(username.value, password.value, email.value);
-    // console.log(email.value);
 }
 
 function callSignup(username, password, email) {
     fetch('http://thesi.generalassemb.ly:8080/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: username, password: password, email: email })
+        body: JSON.stringify({
+            username: username,
+            password: password,
+            email: email
+        })
     })
         .then(res => {
             console.log(res);
-            return res;
+            return res.json();
         })
         .then(res => {
             console.log('Whats inside?');
-            console.log(res.json());
-            token = res.json();
+            token = res.token;
+            console.log(token);
         })
         .catch(error => {
             console.error(error);
@@ -47,8 +41,8 @@ function callSignup(username, password, email) {
 
 function returningUser(e) {
     e.preventDefault();
-    email = document.getElementById('login-email');
-    password = document.getElementById('login-password');
+    let email = document.getElementById('login-email');
+    let password = document.getElementById('login-password');
     console.log(email);
     console.log(password);
     callLogin(email.value, password.value);
@@ -57,14 +51,17 @@ function callLogin(email, password) {
     fetch('http://thesi.generalassemb.ly:8080/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: email, password: password })
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
     })
         .then(res => {
             console.log(res);
             return res;
         })
         .then(res => {
-            console.log(res.json());
+            // console.log(res.json());
             // console.log(res);
             // let temp = res.json();
             // console.log(temp);
